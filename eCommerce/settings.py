e3951281abaaa663387554f51760da94fd847a1f
Ruhul12 <ruhulaminparvez007@gmail.com>
2021-05-05 00:02:26 +0600

    Model Update and Add Products :octocat:

--- eCommerce/settings.py
+++ eCommerce/settings.py
@@ -125,6 +125,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+MEDIA_URL = '/images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- eCommerce/urls.py
+++ eCommerce/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
