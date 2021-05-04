88dab52aebbb4d99673fb203d8e98c62c85bceef
Ruhul12 <ruhulaminparvez007@gmail.com>
2021-05-04 22:55:53 +0600

    Create models for product image octocat

--- store/models.py
+++ store/models.py
@@ -13,6 +13,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(max_length=200, null=True)
     digital = models.BooleanField(default=False, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
