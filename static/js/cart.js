84446b3e4c177bc069652778068a48aad24f3d2c
Ruhul12 <ruhulaminparvez007@gmail.com>
2021-05-10 01:36:41 +0600

    Cart functionality update

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,1 +1,16 @@
-console.log(Hello,World)
+var updateBtns = document.getElementsByClassName('update-cart')
+
+for (i=0; i<updateBtns.length; i++){
+    updateBtns[i].addEventListener('click', function (){
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId:', productId,'Action:', action)
+
+        console.log('USER:', user)
+        if (user === 'AnonymousUser'){
+            console.log('User Not Logged In')
+        }else{
+            console.log('User logged In, Sending data..')
+        }
+    })
+}
