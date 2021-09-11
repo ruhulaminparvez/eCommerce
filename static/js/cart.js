f327366d2b7adb4c5112b15a3c95d674efe8effc
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2021-09-11 13:50:37 +0600

    Fix errors and Add Cart Functionality

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,6 +1,6 @@
 var updateBtns = document.getElementsByClassName('update-cart')
 
-for (i=0; i<updateBtns.length; i++){
+for (var i=0; i<updateBtns.length; i++){
     updateBtns[i].addEventListener('click', function (){
         var productId = this.dataset.product
         var action = this.dataset.action
@@ -10,7 +10,29 @@ for (i=0; i<updateBtns.length; i++){
         if (user === 'AnonymousUser'){
             console.log('User Not Logged In')
         }else{
-            console.log('User logged In, Sending data..')
+            updateUserOrder(productId, action)
         }
+    })
+}
+
+function updateUserOrder(productId, action){
+    console.log('User logged In, Sending data..')
+
+    var url = '/update_item/'
+
+    fetch(url, {
+        method: 'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'X-CSRFToken': csrftoken,  
+        },
+        body:JSON.stringify({'productId':productId, 'action':action})
+    })
+    .then((response) =>{
+        return response.json()
+    })
+    .then((data) =>{
+        console.log('Data', data)
+        location.reload()
     })
 }
--- store/urls.py
+++ store/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
         path('', views.store, name="store"),
         path('cart/', views.cart, name="cart"),
         path('checkout/', views.checkout, name="checkout"),
+        path('update_item/', views.updateItem, name="update_item"),
 
 ]
