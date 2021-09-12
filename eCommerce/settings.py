76c8d814c60e72b89f73025bf84384ebfb7a139c
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2021-09-13 02:30:26 +0600

    Fix Errors

--- eCommerce/settings.py
+++ eCommerce/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
